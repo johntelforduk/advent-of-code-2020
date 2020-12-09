cfb00254443ca0c94218fb169a260716a1f21d85
johntelforduk <jtelford1@gmail.com>
2020-12-09 15:13:17 +0000

    Solution to day 9, part 2.

--- 09-encoding-error/solution.py
+++ 09-encoding-error/solution.py
@@ -16,6 +16,22 @@ def is_pair_sum(pl: list, target: int) -> bool:
     return False
 
 
+def is_contiguous_sum(pl: list, target: int) -> int:
+    """Searches parm list, summing integers starting from first item, and summing items in order trying to equal
+    target. If target equals the contiguous sum, then it returns the sum of the max and min items in the sum.
+    If contiguous sum fails, the function returns -1."""
+    s = 0
+    contig = []
+    for i in pl:
+        s += i
+        contig.append(i)
+        if s == target:
+            return min(contig) + max(contig)
+        if s > target:
+            return -1
+    return -1
+
+
 filename = sys.argv[1]
 preamble = int(sys.argv[2])
 
@@ -25,18 +41,22 @@ if VERBOSE:
 f = open(filename)
 whole_text = (f.read())
 f.close()
-sl = whole_text.split()                    # Split string by any whitespace.
-nl = [int(x) for x in sl]                  # Convert list of strings to list of integers.
+sl = whole_text.split()                     # Split string by any whitespace.
+nl = [int(x) for x in sl]                   # Convert list of strings to list of integers.
 
 if VERBOSE:
     print('nl:', nl)
 
+
+# Part 1.
 available = []                              # List of integers that are available for pairing.
+p1 = None
 
 for this_num in nl:
     if len(available) == preamble:
         if not is_pair_sum(pl=available, target=this_num):
-            print('Part 1:', this_num)
+            p1 = this_num
+            print('Part 1:', p1)
             break
 
     available.append(this_num)
@@ -46,3 +66,17 @@ for this_num in nl:
 
     if VERBOSE:
         print('available:', available)
+
+# Part 2.
+for start in range(len(nl)):
+    if VERBOSE:
+        print('start:', start)
+
+    if VERBOSE:
+        print('nl[start:], p1:', nl[start:], p1)
+
+    x = is_contiguous_sum(nl[start:], target=p1)
+
+    if x != -1:
+        print('Part 2:', x)
+        break
