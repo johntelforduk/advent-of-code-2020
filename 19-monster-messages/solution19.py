fd9f5a917804425454d0e1cd83e5b6d6691d7044
johntelforduk <jtelford1@gmail.com>
2020-12-22 19:26:47 +0000

    Solution to day 19, part 1.

--- 19-monster-messages/solution19.py
+++ 19-monster-messages/solution19.py
@@ -2,12 +2,48 @@
 # https://adventofcode.com/2020/day/19
 
 import sys
+import re                                           # Regular expressions.
 
 VERBOSE = ('-v' in sys.argv)
 
 
-def valid()
+def rule_to_regex(rules: dict, rule_number: str) -> str:
+    """For the parm rule number, return it is a regular expression."""
 
+    rule = rules[rule_number]
+
+    # Base case - single rule which resolves to a literal letter.
+    if len(rule) == 1:
+        if '"' in rule[0]:
+            return rule[0].replace('"', '')         # For example, rule ['"a"'], returns regular expression 'a'.
+
+    # One or more sub-rules.
+    result = '('
+    first = True
+    for each_sub_rule in rule:
+
+        # If there was a preceding sub-rule, delimit this new one with a pipe.
+        if first:
+            first = False                           # Next time around, this will no longer be the first.
+        else:
+            result += '|'
+
+        # result += '('                             # Surround the sub-rule with brackets.
+
+        # Each sub-rule is a string of rule numbers delimited with spaces. For example ['1 2 3'].
+        for each_rule_num in each_sub_rule.split(' '):
+            result += rule_to_regex(rules, rule_number=each_rule_num)
+
+    result += ')'                                   # Close the brackets around this sub-rule.
+    return result
+
+
+def message_match_regexp(message: str, regexp: str) -> bool:
+    """Returns true iff the parm message matches the parm regular expression."""
+
+    return re.match('^'                             # The '^' ensures no leading characters.
+                    + regexp
+                    + '$', message) is not None     # The '$' ensures no trailing characters.
 
 
 def main():
@@ -16,8 +52,30 @@ def main():
     whole_text = f.read()
     f.close()
 
-    rules, messages = whole_text.split('\n\n')
+    rules_raw, messages_raw = whole_text.split('\n\n')
+    rules_list = rules_raw.split('\n')
+    messages = messages_raw.split('\n')
 
+    if VERBOSE:
+        print('rules_list:', rules_list)
+        print('messages:', messages)
+
+    # k = rule number (string).
+    # v = list of sub rules (strings) for that rule number.
+    rules = {}
+    for line in rules_list:
+        rule_number, rule = line.split(': ')
+        rules[rule_number] = rule.split(' | ')
+
+    if VERBOSE:
+        print('rules:', rules)
+
+    regex = rule_to_regex(rules, rule_number='0')
+    part1 = 0
+    for message in messages:
+        if message_match_regexp(message, regex):
+            part1 += 1
+    print('Part 1:', part1)
 
 
 if __name__ == "__main__":
--- 19-monster-messages/test_solution19.py
+++ 19-monster-messages/test_solution19.py
@@ -1,6 +1,6 @@
 # Unit tests for day 19 of AOC 2020, Monster Messages.
 
-from solution19 import
+from solution19 import rule_to_regex, message_match_regexp
 import unittest
 
 
@@ -8,6 +8,33 @@ class TestFunctions(unittest.TestCase):
 
     def test_functions(self):
 
+        test_regex = rule_to_regex(rules={'0': ['"a"']}, rule_number='0')
+        self.assertEqual(test_regex, 'a')
+        self.assertTrue(message_match_regexp(message='a', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='b', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='ab', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='ba', regexp=test_regex))
+
+        test_regex = rule_to_regex(rules={'0': ['1 2 3'], '1': ['4 5'], '4': ['"a"'],
+                                          '5': ['"b"'], '2': ['"c"'], '3': ['"d"']},
+                                   rule_number='0')
+        self.assertTrue(message_match_regexp(message='abcd', regexp=test_regex))
+
+        test_regex = rule_to_regex(rules={'0': ['1 2'], '1': ['"a"'], '2': ['1 3', '3 1'], '3': ['"b"']},
+                                   rule_number='0')
+        self.assertTrue(message_match_regexp(message='aab', regexp=test_regex))
+        self.assertTrue(message_match_regexp(message='aba', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='baa', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='abb', regexp=test_regex))
+
+        test_regex = rule_to_regex(rules={'0': ['4 1 5'], '1': ['2 3', '3 2'], '2': ['4 4', '5 5'],
+                                          '3': ['4 5', '5 4'], '4': ['"a"'], '5': ['"b"']},
+                                   rule_number='0')
+        self.assertTrue(message_match_regexp(message='ababbb', regexp=test_regex))
+        self.assertTrue(message_match_regexp(message='abbbab', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='bababa', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='aaabbb', regexp=test_regex))
+        self.assertFalse(message_match_regexp(message='aaaabbb', regexp=test_regex))
 
 
 if __name__ == '__main__':
